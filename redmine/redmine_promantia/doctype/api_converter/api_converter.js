7ea79ac3116241952335af09be5f72e97e78635a
Yesh <yeshy1643@gmail.com>
2024-02-18 18:17:02 +0530

    redmine-api

---
+++ redmine/redmine_promantia/doctype/api_converter/api_converter.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Mohan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Api converter', {
+	// refresh: function(frm) {
+
+	// }
+});
--- redmine/redmine_promantia/doctype/redmine_timesheet/redmine_timesheet.js
+++ redmine/redmine_promantia/doctype/redmine_timesheet/redmine_timesheet.js
@@ -1,8 +1,25 @@
-// Copyright (c) 2024, Mohan and contributors
-// For license information, please see license.txt
+// // Copyright (c) 2024, Mohan and contributors
 
-// frappe.ui.form.on("Redmine Timesheet", {
-// 	refresh(frm) {
+frappe.ui.form.on('Redmine Timesheet', {
+    refresh(frm) {
+        frm.add_custom_button(__('Upload to Insights'), function() {
+            // Show the progress bar before calling the server method
+            frappe.show_progress('Loading..', 10, 100, 'Uploading to Insights');
+            
+            frm.call('upload_to_insights').then(() => {
+               
+                frappe.hide_progress();
+            });
+        });
+    }
+});
 
-// 	},
-// });
+// Listen for the show_progress and hide_progress events
+frappe.realtime.on("show_progress", function(data) {
+    frappe.show_progress(data.message, data.percent, 100, 'Please wait');
+});
+
+frappe.realtime.on("hide_progress", function(data) {
+    frappe.hide_progress();
+});
+
